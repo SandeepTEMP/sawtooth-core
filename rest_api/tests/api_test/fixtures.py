20ceb7b2507d5ce8281bd051e9a39eb9910cc660
sandeeplandt <sandeepx.hs@intel.com>
2018-09-03 21:40:56 +0530

    Common code for transactions and added 4 tests
    Signed-off-by: sandeeplandt <sandeepx.hs@intel.com>

--- rest_api/tests/api_test/fixtures.py
+++ rest_api/tests/api_test/fixtures.py
@@ -47,213 +47,15 @@ from utils import get_batches,  get_transactions, get_state_address, post_batch,
 
 from payload import get_signer, create_intkey_transaction , create_batch,\
                     create_invalid_intkey_transaction, create_intkey_same_transaction, random_word_list, IntKeyPayload, \
-                    make_intkey_address
+                    make_intkey_address, Transactions
                
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.INFO)
                   
 LIMIT = 100  
          
-class setup_txns:
-         
-    def __init__(self, invalidtype):
-        self.signer = get_signer()
-        self.data = {}
-        self.invalidtype = invalidtype
-        
-    def setup_batch_valinv_txns(self):
-        """Setup method for posting batches and returning the 
-           response
-        """
-        txns = [
-            self.create_intkey_transaction("set",[],30, self.signer),
-            self.create_intkey_transaction("set",[],30, self.signer),
-            self.create_invalid_intkey_transaction("set",[],30, self.signer, self.invalidtype),
-        ]
-        self.data = self.get_txns_commit_data(txns,self.signer, self.data)
-        return self.data
 
-    def setup_batch_invval_txns(self):
-        """Setup method for posting batches and returning the 
-           response
-        """
-        txns = [
-            self.create_invalid_intkey_transaction("set",[],30, self.signer, self.invalidtype),
-            self.create_intkey_transaction("set",[],30, self.signer),
-            self.create_intkey_transaction("set",[],30, self.signer),
-        ]
-        self.data = self.get_txns_commit_data(txns,self.signer, self.data)
-        return self.data
-        
-    def setup_batch_invalid_txns(self):
-        """Setup method for posting batches and returning the 
-           response
-        """  
-          
-        txns = [
-                self.create_invalid_intkey_transaction("set",[],30, self.signer, self.invalidtype),
-                self.create_invalid_intkey_transaction("set",[],30, self.signer, self.invalidtype),
-                self.create_invalid_intkey_transaction("set",[],30, self.signer, self.invalidtype),
-            ]
-
-        self.data = self.get_txns_commit_data(txns,self.signer, self.data)
-        return self.data
-    
-    def setup_batch_valid_one_txns(self):
-        """Setup method for posting batches and returning the 
-           response
-        """
-        txns = [
-            self.create_intkey_transaction("set",[],30, self.signer),
-        ]
-        self.data = self.get_txns_commit_data(txns,self.signer, self.data)
-        return self.data
-    
-    def setup_batch_valid_txns(self):
-        """Setup method for posting batches and returning the 
-           response
-        """
-        txns = [
-            self.create_intkey_transaction("set",[],30, self.signer),
-            self.create_intkey_transaction("set",[],30, self.signer),
-            self.create_intkey_transaction("set",[],30, self.signer),
-        ]
-        self.data = self.get_txns_commit_data(txns,self.signer, self.data)
-        return self.data
-         
-    def get_txns_commit_data(self, txns, signer, data):
-        """Setup method for posting batches and returning the 
-           response
-        """
-        expected_trxn_ids  = []
-        expected_batch_ids = []
-        expected_trxns  = {}
-        expected_batches = []
-        initial_batch_length = batch_count()
-        initial_transaction_length = transaction_count()
-    
-        LOGGER.info("Creating intkey transactions with set operations")
-        
-        for txn in txns:
-            dict = MessageToDict(
-                    txn,
-                    including_default_value_fields=True,
-                    preserving_proto_field_name=True)
-    
-            trxn_id = dict['header_signature']
-            expected_trxn_ids.append(trxn_id)
-        
-        self.data['expected_trxn_ids'] = expected_trxn_ids
-        expected_trxns['trxn_id'] = [dict['header_signature']]
-        expected_trxns['payload'] = [dict['payload']]
-    
-        LOGGER.info("Creating batches for transactions 3trn/batch")
-    
-        batches = [create_batch(txns, signer)]
-        for batch in batches:
-            dict = MessageToDict(
-                    batch,
-                    including_default_value_fields=True,
-                    preserving_proto_field_name=True)
-    
-            batch_id = dict['header_signature']
-            expected_batches.append(batch_id)
-        length_batches = len(expected_batches)
-        length_transactions = len(expected_trxn_ids)
-        
-        post_batch_list = [BatchList(batches=[batch]).SerializeToString() for batch in batches]
-        try:
-            for batch in post_batch_list:
-                response = post_batch(batch)
-                batch_id = dict['header_signature']
-                expected_batches.append(batch_id)
-        except urllib.error.HTTPError as error:
-            LOGGER.info("Rest Api is not reachable")
-            json_data = json.loads(error.fp.read().decode('utf-8'))
-            LOGGER.info(json_data['error']['title'])
-            LOGGER.info(json_data['error']['message']) 
-            LOGGER.info(json_data['error']['code']) 
-      
-        self.data['response'] = response['data'][0]['status'] 
-        self.data['initial_batch_length'] = initial_batch_length
-        self.data['initial_trn_length'] = initial_transaction_length
-        self.data['expected_batch_length'] = initial_batch_length + length_batches
-        self.data['expected_trn_length'] = initial_transaction_length + length_transactions
-        return self.data   
-    
-    def create_intkey_transaction(self, verb, deps, count, signer):
-            words = random_word_list(count)
-            name=random.choice(words)    
-            payload = IntKeyPayload(
-                verb=verb,name=name,value=1)
-
-            addr = make_intkey_address(name)
-            data = self.get_txns_data(addr,deps, payload)
-            return data
-        
-    def create_invalid_intkey_transaction(self, verb, deps, count, signer, invalidtye):
-        words = random_word_list(count)
-        name=random.choice(words) 
-        
-        if invalidtye=="addr":  
-            payload = IntKeyPayload(
-                verb=verb,name=name,value=1)
-            
-            INVALID_INTKEY_ADDRESS_PREFIX = hashlib.sha512(
-            'invalid'.encode('utf-8')).hexdigest()[0:6]
-        
-            addr = INVALID_INTKEY_ADDRESS_PREFIX + hashlib.sha512(
-                name.encode('utf-8')).hexdigest()[-64:]
-            
-        elif invalidtye=="min":   
-            payload = IntKeyPayload(
-                verb=verb,name=name,value=-1)
-            addr = make_intkey_address(name)
-        
-        elif invalidtye=="str":    
-            payload = IntKeyPayload(
-                verb=verb,name=name,value="str")
-            addr = make_intkey_address(name)
-            
-        elif invalidtye=="max":    
-            payload = IntKeyPayload(
-                verb=verb,name=name,value=4294967296)
-            addr = make_intkey_address(name)
-            
-        elif invalidtye=="attr":    
-            payload = IntKeyPayload(
-                verb="verb",name=name,value=1)
-            addr = make_intkey_address(name)
-
-        data = self.get_txns_data(addr,deps, payload)
-        return data
-   
-    def get_txns_data(self, addr, deps, payload):
-    
-        header = TransactionHeader(
-            signer_public_key=self.signer.get_public_key().as_hex(),
-            family_name='intkey',
-            family_version='1.0',
-            inputs=[addr],
-            outputs=[addr],
-            dependencies=deps,
-            payload_sha512=payload.sha512(),
-            batcher_public_key=self.signer.get_public_key().as_hex())
-    
-        header_bytes = header.SerializeToString()
-    
-        signature = self.signer.sign(header_bytes)
-    
-        transaction = Transaction(
-            header=header_bytes,
-            payload=payload.to_cbor(),
-            header_signature=signature)
-    
-        return transaction
-    
-@pytest.fixture(scope="session")
-def Txns():
-        return setup_txns(invalidtype="None")
+data = {}
 
 @pytest.fixture(scope="function")
 def break_genesis(request):
@@ -335,53 +137,33 @@ def invalid_batch():
     
     return data
 
-@pytest.fixture(scope="session")
-def setup_batch_valinv_txns(request):
+@pytest.fixture(scope="function")
+def setup_valinv_txns(request):
     """Setup method for posting batches and returning the 
        response
     """
-    signer = get_signer()
-    data = {}
-    txns = [
-        create_intkey_transaction("set",[],30, signer),
-        create_intkey_transaction("set",[],30, signer),
-        create_invalid_intkey_transaction("set",[],30, signer),
-    ]
-    data = txns_commit_data(txns,signer, data)
+    Txns=Transactions(invalidtype="addr")
+    data = Txns.get_batch_valinv_txns()
     return data
    
-@pytest.fixture(scope="session")
-def setup_batch_invval_txns(request):
+@pytest.fixture(scope="function")
+def setup_invval_txns(request):
     """Setup method for posting batches and returning the 
        response
     """
-    signer = get_signer()
-    data = {}
-    
-    txns = [
-        create_invalid_intkey_transaction("set",[],30, signer),
-        create_intkey_transaction("set",[],30, signer),
-        create_intkey_transaction("set",[],30, signer),
-    ]
-    data = txns_commit_data(txns,signer, data)
+    Txns=Transactions(invalidtype="addr")
+    data = Txns.get_batch_invval_txns()
     return data
-    
 
-@pytest.fixture(scope="session")
-def setup_batch_invalid_txns(request):
+@pytest.fixture(scope="function")
+def setup_invalid_txns(request):
     """Setup method for posting batches and returning the 
        response
     """
-    signer = get_signer()
-    data = {}
-        
-    txns = [
-        create_invalid_intkey_transaction("set",[],30, signer),
-        create_invalid_intkey_transaction("set",[],30, signer),
-        create_invalid_intkey_transaction("set",[],30, signer),
-    ]
-    data = txns_commit_data(txns,signer, data)
+    Txns=Transactions(invalidtype="addr")
+    data = Txns.get_batch_invalid_txns()
     return data
+    
     
 
 @pytest.fixture(scope="function")
@@ -424,67 +206,7 @@ def setup_batch_multiple_transaction():
     
     return expected_trxns
 
-def txns_commit_data(txns, signer, data):
-    """Setup method for posting batches and returning the 
-       response
-    """
-    expected_trxn_ids  = []
-    expected_batch_ids = []
-    expected_trxns  = {}
-    expected_batches = []
-    initial_batch_length = batch_count()
-    initial_transaction_length = transaction_count()
 
-    LOGGER.info("Creating intkey transactions with set operations")
-    
-    for txn in txns:
-        dict = MessageToDict(
-                txn,
-                including_default_value_fields=True,
-                preserving_proto_field_name=True)
-
-        trxn_id = dict['header_signature']
-        expected_trxn_ids.append(trxn_id)
-    
-    data['expected_trxn_ids'] = expected_trxn_ids
-    expected_trxns['trxn_id'] = [dict['header_signature']]
-    expected_trxns['payload'] = [dict['payload']]
-
-
-    LOGGER.info("Creating batches for transactions 3trn/batch")
-
-    batches = [create_batch(txns, signer)]
-    for batch in batches:
-        dict = MessageToDict(
-                batch,
-                including_default_value_fields=True,
-                preserving_proto_field_name=True)
-
-        batch_id = dict['header_signature']
-        expected_batches.append(batch_id)
-    length_batches = len(expected_batches)
-    length_transactions = len(expected_trxn_ids)
-    
-    post_batch_list = [BatchList(batches=[batch]).SerializeToString() for batch in batches]
-    try:
-        for batch in post_batch_list:
-            response = post_batch(batch)
-            batch_id = dict['header_signature']
-            expected_batches.append(batch_id)
-    except urllib.error.HTTPError as error:
-        LOGGER.info("Rest Api is not reachable")
-        json_data = json.loads(error.fp.read().decode('utf-8'))
-        LOGGER.info(json_data['error']['title'])
-        LOGGER.info(json_data['error']['message']) 
-        LOGGER.info(json_data['error']['code']) 
-  
-    data['response'] = response['data'][0]['status'] 
-    data['initial_batch_length'] = initial_batch_length
-    data['initial_trn_length'] = initial_transaction_length
-    data['expected_batch_length'] = initial_batch_length + length_batches
-    data['expected_trn_length'] = initial_transaction_length + length_transactions
-    return data   
-    
 
 
 
--- rest_api/tests/api_test/post/test_rest_api_post.py
+++ rest_api/tests/api_test/post/test_rest_api_post.py
@@ -48,7 +48,7 @@ from payload import get_signer, create_intkey_transaction, create_batch,\
                     create_intkey_same_transaction
 
 from base import RestApiBaseTest
-from fixtures import setup_batch_valinv_txns, setup_batch_invval_txns, setup_batch_invalid_txns, Txns
+from fixtures import setup_valinv_txns, setup_invval_txns, setup_invalid_txns
 
 
 LOGGER = logging.getLogger(__name__)
@@ -372,36 +372,33 @@ class TestPost(RestApiBaseTest):
             assert errcode == 404
 
 class TestPostMulTxns(RestApiBaseTest):
-    def test_txn_invalid_addr(self, Txns):
-        Txns.invalidtype="addr"
-        data=Txns.setup_batch_invalid_txns()
-        initial_batch_length = data['initial_batch_length']
-        expected_batch_length = data['expected_batch_length']
-        initial_trn_length = data['initial_trn_length']
-        expected_trn_length = data['expected_trn_length']
+    def test_txn_invalid_addr(self, setup_invalid_txns):
+        initial_batch_length = setup_invalid_txns['initial_batch_length']
+        expected_batch_length = setup_invalid_txns['expected_batch_length']
+        initial_trn_length = setup_invalid_txns['initial_trn_length']
+        expected_trn_length = setup_invalid_txns['expected_trn_length']
         assert initial_batch_length < expected_batch_length
         assert initial_trn_length < expected_trn_length
-        assert data['response'] == 'INVALID'
+        assert setup_invalid_txns['response'] == 'INVALID'
         
-    def test_txn_valid_invalid_txns(self, Txns):
-        data=Txns.setup_batch_valinv_txns()
-        initial_batch_length = data['initial_batch_length']
-        expected_batch_length = data['expected_batch_length']
-        initial_trn_length = data['initial_trn_length']
-        expected_trn_length = data['expected_trn_length']
+    def test_txn_valid_invalid_txns(self, setup_valinv_txns):
+        #data=Txns.setup_batch_valinv_txns()
+        initial_batch_length = setup_valinv_txns['initial_batch_length']
+        expected_batch_length = setup_valinv_txns['expected_batch_length']
+        initial_trn_length = setup_valinv_txns['initial_trn_length']
+        expected_trn_length = setup_valinv_txns['expected_trn_length']
         assert initial_batch_length < expected_batch_length
         assert initial_trn_length < expected_trn_length
-        assert data['response'] == 'INVALID'
+        assert setup_valinv_txns['response'] == 'INVALID'
         
-    def test_txn_invalid_valid_txns(self, Txns):
-        data=Txns.setup_batch_invval_txns()
-        initial_batch_length = data['initial_batch_length']
-        expected_batch_length = data['expected_batch_length']
-        initial_trn_length = data['initial_trn_length']
-        expected_trn_length = data['expected_trn_length']
+    def test_txn_invalid_valid_txns(self, setup_invval_txns):
+        initial_batch_length = setup_invval_txns['initial_batch_length']
+        expected_batch_length = setup_invval_txns['expected_batch_length']
+        initial_trn_length = setup_invval_txns['initial_trn_length']
+        expected_trn_length = setup_invval_txns['expected_trn_length']
         assert initial_batch_length < expected_batch_length
         assert initial_trn_length < expected_trn_length
-        assert data['response'] == 'INVALID'
+        assert setup_invval_txns['response'] == 'INVALID'
         
   
 
